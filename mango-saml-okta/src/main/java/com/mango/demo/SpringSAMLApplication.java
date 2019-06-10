35443a404d22ab44196ce9c9421965ae5b0b9f8a
Prashant <Prashant@LAPTOP-NCLLRA61>
2019-06-10 07:45:50 +0530

    Initial commit for working auth

---
+++ mango-saml-okta/src/main/java/com/mango/demo/SpringSAMLApplication.java
@@ -0,0 +1,13 @@
+package com.mango.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSAMLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSAMLApplication.class, args);
+	}
+
+}
